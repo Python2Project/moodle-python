f1d2db61f3bf502c6898ae008955898bf1436e87
raissov <brown.robert@bk.ru>
2021-04-30 00:56:07 +0600

    created: Student class

--- moodle/models.py
+++ moodle/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.urls import reverse
 
+
+class Student(models.Model):
+    id = models.AutoField(primary_key=True)
+    email = models.CharField(max_length=255, help_text="Enter an email, please!")
+    firstname = models.CharField(max_length=255, help_text="Enter your First Name, please!")
+    lastname = models.CharField(max_length=255, help_text="Enter your Last Name, please!")
+    groupname = models.CharField(max_length=255, help_text="Enter your email, please!")
+    user_picture = models.FileField(upload_to='images/')
+    password = models.CharField(max_length=255, help_text="Enter your password!")
+
+    def __str__(self):
+        return self.firstname
+
+    def get_absolute_url(self):
+        return reverse('profile', args=[str(self.id)])
 
