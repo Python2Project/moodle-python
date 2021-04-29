710650f2fe1d41190537123ae29e15c7569a96b5
raissov <brown.robert@bk.ru>
2021-04-30 01:03:01 +0600

    created: Teacher model

--- moodle/models.py
+++ moodle/models.py
@@ -8,7 +8,7 @@ class Student(models.Model):
     firstname = models.CharField(max_length=255, help_text="Enter your First Name, please!")
     lastname = models.CharField(max_length=255, help_text="Enter your Last Name, please!")
     groupname = models.CharField(max_length=255, help_text="Enter your email, please!")
-    user_picture = models.FileField(upload_to='images/')
+    user_picture = models.FileField(upload_to='images/student/')
     password = models.CharField(max_length=255, help_text="Enter your password!")
 
     def __str__(self):
@@ -28,3 +28,19 @@ class Course(models.Model):
 
     def get_absolute_url(self):
         return reverse('course', args=[str(self.id)])
+
+
+class Teacher(models.Model):
+    id = models.AutoField(primary_key=True)
+    email = models.CharField(max_length=255, help_text="Enter an email, please!")
+    firstname = models.CharField(max_length=255, help_text="Enter your First Name, please!")
+    lastname = models.CharField(max_length=255, help_text="Enter your Last Name, please!")
+    user_picture = models.FileField(upload_to='images/teacher/')
+    password = models.CharField(max_length=255, help_text="Enter your password!")
+
+    def __str__(self):
+        return self.firstname
+
+    def get_absolute_url(self):
+        return reverse('teacher', args=[str(self.id)])
+
