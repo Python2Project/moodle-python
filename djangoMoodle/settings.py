9802985523aef5fe555bae25507c4539fdaa14f0
raissov <brown.robert@bk.ru>
2021-05-14 02:25:02 +0600

    added: user manager (control over student and teacher models) changed: student and teacher models

--- djangoMoodle/settings.py
+++ djangoMoodle/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'moodle',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -134,3 +135,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'moodle.Student'
--- moodle/models.py
+++ moodle/models.py
@@ -1,13 +1,21 @@
 import jwt
 from datetime import datetime, timedelta
 from django.conf import settings
+from django.core import validators
 from django.db import models
 from django.urls import reverse
+
+from moodle.manager import UserManager
 
 
 class Student(models.Model):
     id = models.AutoField(primary_key=True)
-    email = models.CharField(max_length=255, help_text="Enter an email, please!", null=False, unique=True)
+    username = models.CharField(db_index=True, max_length=255, unique=True)
+    email = models.EmailField(
+        validators=[validators.validate_email],
+        unique=True,
+        blank=False
+    )
     firstname = models.CharField(max_length=255, help_text="Enter your First Name, please!", null=False)
     lastname = models.CharField(max_length=255, help_text="Enter your Last Name, please!", null=False)
     groupname = models.CharField(max_length=255, help_text="Enter your email, please!", null=False)
@@ -18,12 +26,12 @@ class Student(models.Model):
 
     USERNAME_FIELD = 'email'
 
-    REQUIRED_FIELDS = ('email',)
+    REQUIRED_FIELDS = ('username',)
 
     objects = UserManager()
 
     def __str__(self):
-        return self.email
+        return self.username
 
     @property
     def token(self):
@@ -31,6 +39,9 @@ class Student(models.Model):
 
     def get_full_name(self):
         return self.firstname + self.lastname
+
+    def get_short_name(self):
+        return self.username
 
     def _generate_jwt_token(self):
         dt = datetime.now() + timedelta(days=60)
@@ -57,7 +68,12 @@ class Course(models.Model):
 
 class Teacher(models.Model):
     id = models.AutoField(primary_key=True)
-    email = models.CharField(max_length=255, help_text="Enter an email, please!", null=False, unique=True)
+    username = models.CharField(db_index=True, max_length=255, unique=True)
+    email = models.EmailField(
+        validators=[validators.validate_email],
+        unique=True,
+        blank=False
+    )
     firstname = models.CharField(max_length=255, help_text="Enter your First Name, please!", null=False)
     lastname = models.CharField(max_length=255, help_text="Enter your Last Name, please!", null=False)
     user_picture = models.FileField(upload_to='images/teacher/')
@@ -65,8 +81,14 @@ class Teacher(models.Model):
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
 
+    USERNAME_FIELD = 'email'
+
+    REQUIRED_FIELDS = ('username',)
+
+    objects = UserManager()
+
     def __str__(self):
-        return self.email
+        return self.username
 
     @property
     def token(self):
@@ -74,6 +96,9 @@ class Teacher(models.Model):
 
     def get_full_name(self):
         return self.firstname + self.lastname
+
+    def get_short_name(self):
+        return self.username
 
     def _generate_jwt_token(self):
         dt = datetime.now() + timedelta(days=60)
