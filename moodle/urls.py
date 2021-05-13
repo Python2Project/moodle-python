5bf15d39ab82fc6cc8ee1162bf788836a982e691
raissov <brown.robert@bk.ru>
2021-05-14 03:21:38 +0600

    added: login and registration view for student

--- moodle/urls.py
+++ moodle/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
-
+from .views import RegistrationAPIView, LoginAPIView
 
 urlpatterns = [
-
+    re_path(r'^registration/?$', RegistrationAPIView.as_view(), name='user_registration'),
+    re_path(r'^login/?$', LoginAPIView.as_view(), name='user_login'),
 ]
